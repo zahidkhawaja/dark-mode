cd4233a4c1f43d6ce1bb73058cfcb63c01b6033d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-11 16:39:54 -0400

    Add key value to localStorage

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = () => {
-    const [value, setValue] = useLocalStorage();
+    const [value, setValue] = useLocalStorage("darkmode");
 
     useEffect(() => {
 
