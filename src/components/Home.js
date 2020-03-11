e55e9a4bff6c60b25479a6c3f15a562b2da9e98b
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-11 17:33:48 -0400

    Tweak wording, styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Home = () => {
     return (
         <div className = "homepage">
-            <h2>Welcome to the Crypto Tracker!</h2>
+            <h2>Welcome to Crypto Tracker!</h2>
             <div className = "enter">
             <Link to = "/data">Enter</Link>
             </div>
