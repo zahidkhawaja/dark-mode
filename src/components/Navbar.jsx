4f0c3b76866d52732578dddd83532ba6009487a0
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-11 16:47:41 -0400

    Remove param from useDarkMode on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDarkMode } from "../hooks/useDarkMode";
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useDarkMode(false);
+  const [darkMode, setDarkMode] = useDarkMode();
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
